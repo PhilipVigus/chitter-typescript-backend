1001f4a5c9b0b26b16f5bc9c793e2e892858b8c6
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-08 10:31:21 +0100

    Fix ERR_HTTP_HEADERS_SENT error on server
    This was being caused by calling res.send instead of res.end.

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: 5 }).send();
+  res.json({ message: 5 });
+  res.end();
 });
 
 export default app;
