2b08931ad906f8a6a7e692cd32af98c93f48dc21
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-08 15:02:29 +0100

    Add code to test db connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { Client } from "ts-postgres";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,5 +22,33 @@ app.get("/peeps", (req, res) => {
     ]
   });
 });
+
+const dbTest = async () => {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "phil",
+    password: process.env.DB_PASSWORD,
+    database: "chitter_test"
+  });
+
+  try {
+    await client.connect();
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  try {
+    await client.query(
+      "CREATE TABLE test_table (id serial PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL);"
+    );
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    await client.end();
+  }
+};
+
+dbTest();
 
 export default app;
