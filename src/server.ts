109e5af6490171129468ab46a9a6b332bc1d4895
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-08 11:57:03 +0100

    Implement GET peeps route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: 5 });
+});
+
+app.get("/peeps", (req, res) => {
+  res.json({
+    peeps: [
+      { text: "First peep", timeCreated: 1594030856065 },
+      { text: "Second peep", timeCreated: 1494030856065 }
+    ]
+  });
 });
 
 export default app;
