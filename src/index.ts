a417e732c48fa92f24fb24cceea3ff55cb0a29f1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-07-08 09:39:06 +0100

    Add dotenv package and env variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
+
 const app = express();
-const port = 5000;
+const port = process.env.SERVER_PORT;
 
 app.get("/", (req, res) => {
   res.json({ test: 5 }).send();
