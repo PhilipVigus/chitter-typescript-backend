8f99f6a0835c57a8e9be581441f1ae490afa6ed6
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-20 09:20:13 +0100

    Remove duplicate test route

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,6 @@ dotenv.config();
 
 const port = process.env.SERVER_PORT;
 
-app.get("/", (req, res) => {
-  res.json({ test: 5 }).send();
-});
-
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
