068c9aec96963cbe0239c84dcc2f4ba66c59c766
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-20 10:19:48 +0100

    Remove unused body-parser package

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import PGConnection from "./model/PGConnection";
 import peepsRoute from "./routes/peeps";
 
 PGConnection.open();
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
