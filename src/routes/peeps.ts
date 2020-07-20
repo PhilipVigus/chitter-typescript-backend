cbc125019f784cfc956024568c076511195f38cd
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-20 10:35:53 +0100

    Remove unnecessary json call in peeps GET route

--- src/routes/peeps.ts
+++ src/routes/peeps.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import Peep from "../model/Peep";
 
 const router = express.Router();
-router.get("/", async (req, res) => {
-  res.json(await Peep.all());
+router.get("/", async (req: Request, res: Response) => {
+  res.send(await Peep.all());
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req: Request, res: Response) => {
   await Peep.create(req.body.text);
   res.status(200);
   res.end();
