18e230e921fd975495c70c7a81cf6275bb481396
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-21 15:38:14 +0100

    Add test for failed login

--- src/routes/sessionsRoute.ts
+++ src/routes/sessionsRoute.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from "express";
+import PGConnection from "../model/PGConnection";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  res.status(200);
-  res.end();
+  const userData = await PGConnection.query(
+    `SELECT * FROM Users WHERE username='${req.body.username}';`
+  );
+
+  if (userData.rowCount === 0) {
+    res.status(200).send({ error: "error" });
+  } else {
+    res.status(200).send({ id: 1, username: "bob" });
+  }
 });
 
 export default router;
--- src/tests/sessionsRoute.test.ts
+++ src/tests/sessionsRoute.test.ts
@@ -1,13 +1,42 @@
 import request from "supertest";
 import app from "../server";
+import User from "../model/User";
+import PGConnection from "../model/PGConnection";
 
 describe("/sessions endpoint", () => {
+  afterEach(async () => {
+    await PGConnection.query("TRUNCATE Users;");
+  });
+
+  afterAll(async () => {
+    await PGConnection.close();
+  });
+
   describe("POST", () => {
     it("returns status 200", async () => {
+      User.create("bob", "12345678");
       const res = await request(app)
         .post("/sessions")
         .send({ username: "bob", password: "12345678" });
+
       expect(res.status).toBe(200);
+    });
+
+    it("returns the username and id", async () => {
+      User.create("bob", "12345678");
+      const res = await request(app)
+        .post("/sessions")
+        .send({ username: "bob", password: "12345678" });
+
+      expect(res.body).toEqual({ id: 1, username: "bob" });
+    });
+
+    it("returns an error if the login fails", async () => {
+      const res = await request(app)
+        .post("/sessions")
+        .send({ username: "bob", password: "12345678" });
+
+      expect(res.body).toEqual({ error: "error" });
     });
   });
 });
