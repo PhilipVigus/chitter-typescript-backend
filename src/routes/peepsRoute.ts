5b56de78311b14b8bce67f768263a5423747039a
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-23 15:27:06 +0100

    Implement sending auth details as cookies to client
    The server currently just sends the username and id when you
    successfully login. This will be built on with later work.

--- src/routes/peepsRoute.ts
+++ src/routes/peepsRoute.ts
@@ -2,7 +2,19 @@ import express, { Request, Response } from "express";
 import Peep from "../model/Peep";
 
 const router = express.Router();
+
 router.get("/", async (req: Request, res: Response) => {
+  console.log("in peeps");
+  if (req.session) {
+    console.log("session");
+    console.log(req.session.test);
+  }
+
+  if (req.cookies) {
+    console.log("cookies");
+    console.log(req.cookies);
+  }
+
   res.send(await Peep.all());
 });
 
