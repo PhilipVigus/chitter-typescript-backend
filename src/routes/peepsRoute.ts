d1b06766ed1d429dc5cdac5c6494dbf95595e4c9
PhilipVigus <philipvigus@yahoo.co.uk>
2020-07-24 14:30:15 +0100

    Fix node version clash warning

--- src/routes/peepsRoute.ts
+++ src/routes/peepsRoute.ts
@@ -4,17 +4,6 @@ import Peep from "../model/Peep";
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  console.log("in peeps");
-  if (req.session) {
-    console.log("session");
-    console.log(req.session.test);
-  }
-
-  if (req.cookies) {
-    console.log("cookies");
-    console.log(req.cookies);
-  }
-
   res.send(await Peep.all());
 });
 
